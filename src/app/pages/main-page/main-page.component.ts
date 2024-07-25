54ea4a2aac87aadb252383df9762df58f7ddf18e
Yaroslav Pidhornyi <devinctus.seo@gmail.com>
2024-07-25 19:03:16 +0300

    Added main page component (dashboard)

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ApplicantService } from '../../services/applicant.service';
+import { EmployerService } from '../../services/employer.service';
+import { VacancyService } from '../../services/vacancy.service';
+import { AgreementService } from '../../services/agreement.service';
+
+import { IApplicant } from '../../models/applicant';
+import { IEmployer } from '../../models/employer';
+import { IVacancy } from '../../models/vacancy';
+import { IAgreement } from '../../models/agreement';
 
 @Component({
-  selector: 'app-main-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+    standalone: true,
+    selector: 'app-main-page',
+    templateUrl: './main-page.component.html',
+    styleUrls: ['./main-page.component.scss'],
+    imports: [
+        CommonModule,
+        MatCardModule,
+        MatButtonModule,
+        MatIconModule,
+        RouterModule,
+    ],
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+    recentApplicants: IApplicant[] = [];
+    recentEmployers: IEmployer[] = [];
+    recentVacancies: IVacancy[] = [];
+    recentAgreements: IAgreement[] = [];
 
+    constructor(
+        private applicantService: ApplicantService,
+        private employerService: EmployerService,
+        private vacancyService: VacancyService,
+        private agreementService: AgreementService,
+    ) {}
+
+    ngOnInit(): void {
+        this.loadRecentData();
+    }
+
+    loadRecentData(): void {
+        this.applicantService.getAll().subscribe((data: IApplicant[]) => {
+            this.recentApplicants = data.slice(-5).reverse();
+        });
+
+        this.employerService.getAll().subscribe((data: IEmployer[]) => {
+            this.recentEmployers = data.slice(-5).reverse();
+        });
+
+        this.vacancyService.getAll().subscribe((data: IVacancy[]) => {
+            this.recentVacancies = data.slice(-5).reverse();
+        });
+
+        this.agreementService.getAll().subscribe((data: IAgreement[]) => {
+            this.recentAgreements = data.slice(-5).reverse();
+        });
+    }
 }
